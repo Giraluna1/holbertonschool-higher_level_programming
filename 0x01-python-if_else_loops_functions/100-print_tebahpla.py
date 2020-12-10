825ae4bdb8e4b93997116b9f8b774e82a9895523
Giraluna1 <lunagolo@hotmail.com>
2020-12-09 23:21:44 -0500

    add alphabet reversed

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+altern = ''
+for i in reversed(range(97, 123)):
+    if (i != 113) and (i != 101):
+        if not i % 2:
+            altern = chr(i).lower()
+        else:
+            altern = chr(i).upper()   
+        print('{}'.format(altern), end="")
