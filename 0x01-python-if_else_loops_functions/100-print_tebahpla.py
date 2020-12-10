223f8ad97d89f19f90a7f02d3e57fbe0c24a8d54
Giraluna1 <lunagolo@hotmail.com>
2020-12-09 23:24:58 -0500

    Fix pep8

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,5 +5,5 @@ for i in reversed(range(97, 123)):
         if not i % 2:
             altern = chr(i).lower()
         else:
-            altern = chr(i).upper()   
+            altern = chr(i).upper()
         print('{}'.format(altern), end="")
