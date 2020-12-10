f2f37887361269fc2d6f3a1270b7bd9cdfe3288b
Giraluna1 <lunagolo@hotmail.com>
2020-12-10 17:20:55 -0500

    fix count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,16 @@ if __name__ == "__main__":
     import sys
     num_arguments = len(argv)
     argument = 1
-    
+
     if num_arguments < 1:
         print('{} arguments.'.format(num_arguments-1))
     elif num_arguments == 2:
         print('{} argument:'.format(num_arguments-1))
     else:
-        print('{} arguments.'.format(num_arguments-1))
-            
+        print('{} arguments:'.format(num_arguments-1))
+
+    i = 1
+
     for argument in range(1, len(sys.argv)):
-       print(sys.argv[argument])
+        print('{:d}: {}'.format(i, sys.argv[argument]))
+        i += 1
