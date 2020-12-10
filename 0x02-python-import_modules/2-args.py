a70b78f962d698a78865dd230adad81315283516
Giraluna1 <lunagolo@hotmail.com>
2020-12-10 17:10:50 -0500

    add arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    import sys
+    num_arguments = len(argv)
+    argument = 1
+    
+    if num_arguments < 1:
+        print('{} arguments.'.format(num_arguments-1))
+    elif num_arguments == 2:
+        print('{} argument:'.format(num_arguments-1))
+    else:
+        print('{} arguments.'.format(num_arguments-1))
+            
+    for argument in range(1, len(sys.argv)):
+       print(sys.argv[argument])
