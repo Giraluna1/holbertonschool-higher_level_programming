283f7d215c24c22539aba50ec249f0f68bd1dff3
Giraluna1 <lunagolo@hotmail.com>
2020-12-17 11:09:29 -0500

    Add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    a = ((tuple_a[0] if len_a >= 1 else 0) + (tuple_b[0] if len_b >= 1 else 0))
+    b = ((tuple_a[1] if len_a >= 2 else 0) + (tuple_b[1] if len_b >= 2 else 0))
+    add = a, b
+    return add
