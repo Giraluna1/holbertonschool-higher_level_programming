ede6fbddc01865f6ff2b2569fa2c6783345b01c2
Giraluna1 <lunagolo@hotmail.com>
2020-12-18 12:34:43 -0500

    values multiplies by2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dicty = dict(a_dictionary)
+    dicty = sorted(a_dictionary.keys())
+    for item in dicty:
+        new_dicty[item] = a_dictionary[item] * 2
+    return new_dicty
