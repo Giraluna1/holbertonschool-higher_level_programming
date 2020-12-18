58815665e237dc293af4e50433769191cc664f9f
Giraluna1 <lunagolo@hotmail.com>
2020-12-18 11:39:09 -0500

    sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted(a_dictionary.keys())
+    # ordenamos el diccionario en lista de tuplas
+    dicty = sorted(a_dictionary.keys())
+    for item in dicty:
+        print('{}: {}'.format(item, a_dictionary[item]))
