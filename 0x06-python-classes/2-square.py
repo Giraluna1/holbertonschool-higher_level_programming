f0e3906bbe02407cf734c190bf32b45fe5981ab1
Giraluna1 <lunagolo@hotmail.com>
2021-01-05 23:01:43 -0500

    add options

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,8 +20,8 @@ class Square():
             size (int): size of the square.
         """
         if type(size) is not int:
-            raise TypeError("size must be a integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size mut be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
