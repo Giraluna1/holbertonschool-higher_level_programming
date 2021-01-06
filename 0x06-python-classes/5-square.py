c64789391b6aab930d412b6e00b33c60cf7ba9c9
Giraluna1 <lunagolo@hotmail.com>
2021-01-06 15:28:25 -0500

    Add square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Square Class documentation
+
+ """
+
+
+class Square():
+    """  Size of Square.
+
+    Private instance attribute:
+        __size
+    Instantiation with size (no type/value verification).
+
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """ __init method size.
+        Args:
+            size (int): size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """obtain size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ set the value.
+        Args:
+            value : value of the size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ method area.
+        Args:
+            None
+        Return:
+            Area of square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ method print square
+        Return:
+            figure square with the character #
+        """
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print('#', end='')
+            print()
