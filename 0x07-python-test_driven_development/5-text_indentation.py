a5dc2f5cf9b7cd75aca0cdaa57e6e59c652d33fe
Giraluna1 <lunagolo@hotmail.com>
2021-01-10 23:54:39 -0500

    text identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is he module Text indetation
+
+"""
+
+
+def text_indentation(text):
+    """
+    print a text with 2 new lines after each of these characters
+
+    """
+
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    else:
+        replace_dot1 = text.replace('. ', '.')
+        replace_dot2 = replace_dot1.replace('.', '.\n\n')
+        replace_sigQ1 = replace_dot2.replace('? ', '?')
+        replace_sigQ2 = replace_sigQ1.replace('?', '?\n\n')
+        replace_2dot1 = replace_sigQ2.replace(': ', ':')
+        replace_2dot2 = replace_2dot1.replace(':', ':\n\n')
+
+        print(replace_2dot2)
