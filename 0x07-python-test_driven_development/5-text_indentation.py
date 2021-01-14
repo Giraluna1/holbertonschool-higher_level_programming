f919859bd5dc31efc0dea892c58aa96c5293908d
Giraluna1 <lunagolo@hotmail.com>
2021-01-14 13:01:40 -0500

    fix code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,14 +10,15 @@ def text_indentation(text):
     print a text with 2 new lines after each of these characters
 
     """
-    identation_text = ""
+
     if type(text) is not str:
         raise TypeError('text must be a string')
     else:
+
         replace_dot1 = text.replace('. ', '.')
         replace_dot2 = replace_dot1.replace('.', '.\n\n')
         replace_sigQ1 = replace_dot2.replace('? ', '?')
         replace_sigQ2 = replace_sigQ1.replace('?', '?\n\n')
         replace_2dot1 = replace_sigQ2.replace(': ', ':')
         replace_2dot2 = replace_2dot1.replace(':', ':\n\n')
-        print(replace_2dot2, end='')
+        print(replace_2dot2.strip(), end='')
