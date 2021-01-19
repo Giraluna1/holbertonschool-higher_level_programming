0d64c535af28c919081d93d9d5a7cfc4cbe6ad5d
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 16:22:10 -0500

    Add function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+""" This is module Exact same Object """
+
+
+def is_same_class(obj, a_class):
+    """ Function check is the same class
+    Args:
+        obj (int): object
+        a_class (objec): (wherever you want): any class
+    Return:
+        True if the object is exactly an instance
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
