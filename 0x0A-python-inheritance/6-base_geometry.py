b801dca95683509306454b82a3662d86448ca1e5
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 17:23:07 -0500

    Add public method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+""" Module Improve Geometry """
+
+
+class BaseGeometry():
+    """ Base Geometry class have:
+    Method Area: that raise an exception
+    """
+
+    def area(self):
+        """ Public method that raise an Exception """
+        raise Exception('area() is not implemented')
