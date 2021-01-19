0efd9b1664fda9940a37de6425108a6ac47bc8f8
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 15:21:59 -0500

    Add sorted function

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+""" This is the Module My list """
+
+
+class Mylist(list):
+    """ inherits from list class """
+
+    def print_sorted(self):
+        """Method that prints the list, but sorted (ascending sort)
+        """
+        List_sorted = sorted(self)
+        print(List_sorted)
