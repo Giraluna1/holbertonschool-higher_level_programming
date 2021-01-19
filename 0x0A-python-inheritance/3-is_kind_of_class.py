64a659267c5b21f21d15a784f3635da11d5c3841
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 16:27:55 -0500

    Add function with isistance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+""" This is the mode Same class or inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """function that check is inherited class
+    Args:
+        obj (int): object type integer
+        a_class (wherever type): any class
+    Return:
+        bool: True o False
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
