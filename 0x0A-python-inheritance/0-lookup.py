ac4e6b355a426aaabf3a31e0cd54bfa1f2fc4218
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 14:23:32 -0500

    Add lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+""" Module Lookup """
+
+
+def lookup(obj):
+    """ Function
+    Args:
+        obj: Object
+    Return: the list of available attributes and methods of an object
+    """
+    return list(dir(obj))
