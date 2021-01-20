86a96dde818ce7cff4283056d273951fddb9437e
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 23:16:25 -0500

    Add method area and __str__

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Module Rectangle """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ class that inherits from BaseGeomety """
+
+    def __init__(self, width, height):
+        """ instantation constructor
+        Args:
+        width (int): width of a rectangle
+        height (int): height of a rectangle
+
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ return: the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """return: an print rectangle description:
+            [Rectangle] <width>/<height>
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+r = Rectangle(3, 5)
+
+print(r)
+print(r.area())
