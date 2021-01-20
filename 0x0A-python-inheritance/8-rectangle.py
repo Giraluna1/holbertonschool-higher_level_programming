b80578bd5ea7939a9c53d0c9e046b3e5f16133b5
Giraluna1 <lunagolo@hotmail.com>
2021-01-19 22:40:37 -0500

    Add subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
 """ Module Rectangle """
 
-class BaseGeometry():
-    """ Base Geometry class have:
-    Method Area: that raise an exception
-    Methoss integer validator: that validates values
-    """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        """ Public method that raise an Exception """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """ Public method that validates value
-            if the value is not a integer: raise Type Error
-            if the value is less or equal to 0: raise a Value Error
-        """
-        self.value = value
-        self.name = name
-        if type(self.value) is not int:
-            raise TypeError('{} must be an integer'.format(self.name))
-        if self.value <= 0:
-            raise ValueError('{} must be greater than 0'.format(self.name))
 
 class Rectangle(BaseGeometry):
-""" class that inherits from BaseGeomety """
+    """ class that inherits from BaseGeomety """
 
     __width = 0
     __height = 0
 
     def __init__(self, width, height):
-        """ instantation """
-        self.__width = width
-        self.__height = height
+        """ instantation constructor
+        Args:
+        width (int): width of a rectangle
+        height (int): height of a rectangle
 
-        if integer_validator(width, s)
-
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        self.__width
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__height
