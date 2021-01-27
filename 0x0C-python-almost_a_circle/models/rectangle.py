712b91519410ba28aeace3544332817d9895a322
Giraluna1 <lunagolo@hotmail.com>
2021-01-27 12:23:46 -0500

    Fix typeErro Wisht height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,7 +34,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('width must be > 0')
         else:
             self.__width = value
 
@@ -51,7 +51,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError('height must be > 0')
         else:
             self.__height = value
 
