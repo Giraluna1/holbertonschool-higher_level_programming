46d02be962894ca2e7f91da2fb8e5452e6a9c0b2
Giraluna1 <lunagolo@hotmail.com>
2021-03-01 17:06:05 -0500

    Fix put the rows second table and add multiples records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table(
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES
+INSERT INTO second_table (id, name, score) VALUES
     (1, "Jhon", 10),
     (2, "Alex", 3),
     (3, "Bob", 14),
