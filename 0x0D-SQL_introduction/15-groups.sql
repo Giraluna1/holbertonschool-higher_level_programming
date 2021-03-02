e9a4e98018e0808906ec26145b036efe533099a5
Giraluna1 <lunagolo@hotmail.com>
2021-03-01 19:02:29 -0500

    Fix order the records

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,7 @@
 -- This script lists the numbes of records with
 -- the same score in the scond table
-SELECT score COUNT(score) 'number', score
+SELECT score AS "score",
+    COUNT(score) AS 'number'
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
