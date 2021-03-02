c7f994ebe3486d696f3c759c9d6cf53de8aecbae
Giraluna1 <lunagolo@hotmail.com>
2021-03-01 19:00:37 -0500

    Count the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- This script lists the numbes of records with
 -- the same score in the scond table
-SELECT COUNT(score) 'number', score
+SELECT score COUNT(score) 'number', score
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
