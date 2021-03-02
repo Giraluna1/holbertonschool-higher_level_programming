ec9208800546ecc1b7b5450cbc958d4af47aa0ac
Giraluna1 <lunagolo@hotmail.com>
2021-03-01 23:44:46 -0500

    Fix name max

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- This script display the max temperatue of each state
 -- order by state name
-SELECT state, MAX(value) 'max_value' FROM temperatures
+SELECT state, MAX(value) 'max_temp' FROM temperatures
 GROUP BY state
 ORDER BY state;
