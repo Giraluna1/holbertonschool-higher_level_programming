6b6d431cb025f117668d4304800dfafa617aef54
Giraluna1 <lunagolo@hotmail.com>
2021-03-02 23:42:52 -0500

    Add LEFT OUTER JOIN

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- This script join tv_showa and tv_shows_genres without a genre linked.
+-- tittle - genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
