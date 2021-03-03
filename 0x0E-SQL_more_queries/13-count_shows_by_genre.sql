8bc2e5da3b1669c687978e5ffaa1c7e8895714b5
Giraluna1 <lunagolo@hotmail.com>
2021-03-03 00:25:10 -0500

    Fiz Alias genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,9 +5,9 @@
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, COUNT(tv_genres.id) AS number_of_shows
+SELECT tv_genres.name 'genre', COUNT(tv_genres.id) AS number_of_shows
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
