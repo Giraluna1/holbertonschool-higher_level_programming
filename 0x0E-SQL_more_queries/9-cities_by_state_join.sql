2d932274ee4bc1ba2805087cd13f2a70a38f1f5d
Giraluna1 <lunagolo@hotmail.com>
2021-03-02 22:52:55 -0500

    show all cities containes in the database

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- this script lists all cities contained
+-- in the hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
