3e6f9cc3ba11d67354232cec1d95ed7b31309c1a
Giraluna1 <lunagolo@hotmail.com>
2021-03-02 22:41:01 -0500

    Show olnly cities in California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- this scrip lists all the cities of California
 -- that can be found in the database hbtn_0d_usa
-SELECT states.name FROM states
-WHERE name = California
-INNER JOIN cities
-ON cities.state_id = state.id
+SELECT id, name FROM cities
+WHERE state_id IN
+    (SELECT id FROM states WHERE name = 'California')
