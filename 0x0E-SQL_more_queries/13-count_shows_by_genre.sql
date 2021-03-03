8f38fa2bb4556416fd2850fbc9b811fb6a36e73a
Giraluna1 <lunagolo@hotmail.com>
2021-03-03 00:23:01 -0500

    Add Inner

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,7 +7,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name, COUNT(tv_genres.id) AS number_of_shows
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY genre_id
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
