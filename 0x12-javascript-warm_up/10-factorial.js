e5aa9afe46df5191e47a057601139edbac302223
Giraluna1 <lunagolo@hotmail.com>
2021-03-22 22:13:40 -0500

    Fix the is nan control

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const firstInt = parseInt(process.argv[2]);
 
 function factorial (num) {
-  if (num === 0 || num === 1) {
+  if (num === 0 || num === 1 || isNaN(num)) {
     return 1;
   } else {
     return num * factorial(num - 1);
