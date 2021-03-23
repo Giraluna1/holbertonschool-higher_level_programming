11eb1d9ee68b7c0730f68c7eeb0786f524f97bc2
Giraluna1 <lunagolo@hotmail.com>
2021-03-22 20:29:52 -0500

    Change control for isNaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const argument1 = process.argv[2];
 
-if (typeof argument1 !== 'number') {
+if (isNaN(argument1)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(argument1));
