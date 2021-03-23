08e81a33204f0a6cdfb8afd8b5ba60956d120f9e
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 18:04:13 -0500

    Add two new mwthods, rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let item = 0; item < this.height; item++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let lastWidth = this.width
+    let lastHeight = this.height
+
+    this.height = lastWidth
+    this.width = lastHeight
+  }
+
+  double () {
+    let lastWidth = this.width
+    let lastHeight = this.height
+
+    this.width = lastWidth * 2
+    this.height = lastHeight * 2
+  }
+}
+
+module.exports = Rectangle;
