2d4ccd25a29d92aa5c95488927dc83507e09cbd9
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 17:52:39 -0500

    Add method print()

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let item = 0; item < this.height; item++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
