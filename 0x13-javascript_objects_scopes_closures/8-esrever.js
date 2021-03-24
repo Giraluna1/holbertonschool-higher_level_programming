136dfad171e417391da1a9d3549e5a1d7e4675df
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 22:59:00 -0500

    Add function reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    return list.reverse();
+    const newList = [];
+    for (let item = list.length - 1; item >= 0; item--) {
+        newList.push(list[item]);
+    }
+    return newList;
 }
