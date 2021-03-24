717114a8a90798c0834fe977cda295b883273f28
Giraluna1 <lunagolo@hotmail.com>
2021-03-24 15:12:55 -0500

    Change <=

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const sorted = new Float64Array(process.argv.slice(2).sort());
 console.log(sorted);
 
-if (process.argv.length < 4) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
   console.log(sorted[sorted.length - 2]);
