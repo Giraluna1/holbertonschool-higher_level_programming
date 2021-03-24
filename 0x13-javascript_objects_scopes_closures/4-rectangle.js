72c590d4c33dfdd8fa2c50ae7ab5b92811e86fe0
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 19:06:33 -0500

    Fix chang let

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,21 +9,21 @@ class Rectangle {
   }
 
   print () {
-    for (let item = 0; item < this.height; item++) {
+    for (const item = 0; item < this.height; item++) {
       console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    let lastWidth = this.width;
-    let lastHeight = this.height;
+    const lastWidth = this.width;
+    const lastHeight = this.height;
     this.height = lastWidth;
     this.width = lastHeight;
   }
 
   double () {
-    let lastWidth = this.width;
-    let lastHeight = this.height;
+    const lastWidth = this.width;
+    const lastHeight = this.height;
     this.width *= 2;
     this.height *= 2;
   }
