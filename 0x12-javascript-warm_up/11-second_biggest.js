cd9d2fe301de2ea5c523e2f28af4c9951dec76d4
Giraluna1 <lunagolo@hotmail.com>
2021-03-24 15:21:31 -0500

    Convert to a integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const sorted = new Float64Array(process.argv.slice(2).sort());
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const secondBigest = Number(sorted[sorted.length - 2]);
+  const secondBigest = `${Number(sorted[sorted.length - 2])}`;
   console.log(secondBigest);
 }
