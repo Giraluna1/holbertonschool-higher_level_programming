e0ea9af0dbe1a7afaf7c49dadc2c90e234d09b56
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 22:27:21 -0500

    Fixed semi

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let counter = 0;
-    for (element of list) {
-        if (element === searchElement) {
-            counter += 1;
-        }
+  let counter = 0;
+
+  for (const element of list) {
+    if (element === searchElement) {
+      counter += 1;
     }
-    return counter;
-}
+  }
+  return counter;
+};
