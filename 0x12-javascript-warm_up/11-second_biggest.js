e98ae12b9d393ff71f36e204cec221bfa4c1e840
Giraluna1 <lunagolo@hotmail.com>
2021-03-24 14:55:26 -0500

    Add script that searches the second biggest num

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const sorted = new Float64Array(process.argv.slice(2).sort());
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  console.log(sorted[sorted.length - 2]);
+}
