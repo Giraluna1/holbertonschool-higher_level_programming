352c22f6bbd26339c657c3c443b264b2fcfa582a
Giraluna1 <lunagolo@hotmail.com>
2021-03-23 23:02:06 -0500

    fix the semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const newList = [];
+  const newList = [];
     for (let item = list.length - 1; item >= 0; item--) {
-        newList.push(list[item]);
+      newList.push(list[item]);
     }
     return newList;
-}
+};
