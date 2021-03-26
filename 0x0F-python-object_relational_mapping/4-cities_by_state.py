51717a7dc107ce07f4c3d5e14dc36891453c8108
Giraluna1 <lunagolo@hotmail.com>
2021-03-26 14:45:01 -0500

    List all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Filter states by user input """
+
+import MySQLdb
+import sys
+
+mysql_username = sys.argv[1]
+database_name = sys.argv[2]
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localHost", port=3306,
+                           user=mysql_username, passwd='',
+                           db=database_name, charset="utf8")
+    cur = conn.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name FROM states \
+         INNER JOIN cities ON states.id = cities.state_id \
+         ORDER BY cities.id ASC")
+    full_cities = cur.fetchall()
+    for row in full_cities:
+        print(row)
+    cur.close()
+    conn.close()
