6c41ca6f53ead8016bbdf8ef149b94e233e0ac77
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 17:01:48 -0500

    Delete the variables

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,19 +5,14 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_to_search = sys.argv[4]
-
     conn = MySQLdb.connect(host="localHost", port=3306,
-                           user=mysql_username, passwd=mysql_password,
-                           db=database_name, charset="utf8")
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute(
         "SELECT * FROM states WHERE \
-         name=%s ORDER BY id ASC", (state_to_search, ))
+         name=%s ORDER BY id ASC", (sys.argv[4], ))
     full_states = cur.fetchall()
     for row in full_states:
         print(row)
