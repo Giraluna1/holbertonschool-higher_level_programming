0dbe85200d5b4160bb050080de1d351550c6dec9
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 11:59:49 -0500

    add trhid argument

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localHost", port=3306,
-                           user=sys.argv[1], passwd='',
-                           db=sys.argv[2], charset="utf8")
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute("SELECT * FROM states ORDER BY id ASC")
