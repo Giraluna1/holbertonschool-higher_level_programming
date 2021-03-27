4a4e0406153ea0705b8da37252146aa8351f9481
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 17:27:05 -0500

    Change H of localhost

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ mysql_username = sys.argv[1]
 mysql_password = sys.argv[2]
 database_name = sys.argv[3]
 
-conn = MySQLdb.connect(host="localHost", port=3306,
+conn = MySQLdb.connect(host="localhost", port=3306,
                        user=mysql_username, passwd=mysql_password,
                        db=database_name, charset="utf8")
 cur = conn.cursor()
