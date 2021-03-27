aa9804b224a5d51376c74ebf07351b6294423a0a
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 18:16:25 -0500

    Add main

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-""" First state """
+"""
+lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa
+"""
 import sys
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-# import the layout of the table instead of Base class
-from model_state import State, Base
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+if __name__ == "__main__":
+    # import the layout of the table instead of Base class
+    from model_state import State, Base
 
-# Accesing the MetaData
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.
-    format(mysql_username, mysql_password, database_name), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-# create the session
-session = Session(engine)
+    # Accesing the MetaData
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(mysql_username, mysql_password, database_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-# The query: lists all State objects that
-# contain the letter a from the database hbtn_0e_6_usa
-for state in session.query(State).filter(State.name.like('%a%')):
-    print("{}: {}".format(state.id, state.name))
+    # create the session
+    session = Session(engine)
 
-session.close()
+    # The query: lists all State objects that
+    # contain the letter a from the database hbtn_0e_6_usa
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
