4f0a42eec1e7286c54aae0c7fbb592211f24fcc7
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 14:54:35 -0500

    Add password argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,13 +5,14 @@ import MySQLdb
 import sys
 
 mysql_username = sys.argv[1]
-database_name = sys.argv[2]
-state_name = sys.argv[3]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+state_name = sys.argv[4]
 
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localHost", port=3306,
-                           user=mysql_username, passwd='',
+                           user=mysql_username, passwd=mysql_password,
                            db=database_name, charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
