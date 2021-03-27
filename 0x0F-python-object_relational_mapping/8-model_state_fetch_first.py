3fb71442299e5cafba7c1b78e7b59b9198e2c7d4
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 18:08:18 -0500

    Add main

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,24 +6,28 @@ import sys
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-# import the layout of the table instead of Base class
-from model_state import State, Base
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+if __name__ == "__main__":
+    # import the layout of the table instead of Base class
+    from model_state import State, Base
 
-# Accesing the MetaData
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.format(mysql_username, mysql_password, database_name), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-# create the session
-session = Session(engine)
+    # Accesing the MetaData
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(mysql_username, mysql_password, database_name),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-for state in session.query(State).order_by(State.id).filter(State.id == 1):
-    if state is not None:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
-session.close()
+    # create the session
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).filter(State.id == 1):
+        if state is not None:
+            print("{}: {}".format(state.id, state.name))
+        else:
+            print("Nothing")
+    session.close()
