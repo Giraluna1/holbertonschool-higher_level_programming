762d00cc1e2a4d3b8f50d073b62da6549f8646de
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 16:11:00 -0500

    fixed the docstring

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Filter states
+""" Filter states write a scrip that list all states
+    with a name satarting with N (upper) from the databases
 """
 
 import sys
@@ -11,7 +12,6 @@ if __name__ == "__main__":
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-
     cur.execute(
         "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
