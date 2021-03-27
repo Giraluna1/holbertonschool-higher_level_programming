d7b9bc913f8db8f01d689c5789e87f6973def453
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 16:26:28 -0500

    Try code sebas

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
 Filter states write a scrip that list all states
-with a name satarting with N (upper) from the databases
+with a name satarting with N (upper) from the databases hbtn.
 """
 
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localHost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2],
-                           db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\
+        WHERE states.name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
