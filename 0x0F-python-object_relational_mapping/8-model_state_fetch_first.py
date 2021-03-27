9c3954239c3293e0acd29a4a7fe57678fc1431ba
Giraluna1 <lunagolo@hotmail.com>
2021-03-26 22:15:22 -0500

    Query for the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" First state """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+# import the layout of the table instead of Base class
+from model_state import State, Base
+
+mysql_username = sys.argv[1]
+database_name = sys.argv[2]
+
+# Accesing the MetaData
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'.format(mysql_username, "", database_name), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+# create the session
+session = Session(engine)
+
+for state in session.query(State).order_by(State.id).filter(State.id == 1):
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+session.close()
