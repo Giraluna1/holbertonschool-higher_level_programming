0548e4cda612396d38b7288d1fc6725323bb8689
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 17:57:00 -0500

    Change ordeer of the tables

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
                            passwd=mysql_password, db=database_name, charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states\
-            JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+            JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     full_cities = cur.fetchall()
     for row in full_cities:
         print(row)
