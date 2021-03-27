30b06fea498998134363edbb90fa1a926d2f038c
Giraluna1 <lunagolo@hotmail.com>
2021-03-26 21:36:47 -0500

    Add All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" All satates via SQLAlchemy """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+# import the layout of the table instead of Base class
+from model_state import State, Base
+
+mysql_username = sys.argv[1]
+database_name = sys.argv[2]
+
+# Accesing the MetaData
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'.format(mysql_username, "", database_name), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+# create the session
+session = Session(engine)
+
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
