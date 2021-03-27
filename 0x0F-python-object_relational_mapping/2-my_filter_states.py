65c7ee9e92f4f2c102453a413da09a284b0eb470
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 16:57:13 -0500

    Add line 16 of sebas2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,17 +8,13 @@ import sys
 
 if __name__ == "__main__":
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    conn = MySQLdb.connect(host="localHost", port=3306,
-                           user=mysql_username, passwd=mysql_password,
-                           db=database_name, charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute("SELECT * FROM states\
-        WHERE states.name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+            WHERE states.name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     full_states = cur.fetchall()
     for row in full_states:
         print(row)
