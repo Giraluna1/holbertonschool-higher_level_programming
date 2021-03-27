667afcd4f8a403af464b3deacd13e6921444f2b7
Giraluna1 <lunagolo@hotmail.com>
2021-03-26 22:55:54 -0500

    Add query taking a fourth argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" First state """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+# import the layout of the table instead of Base class
+from model_state import State, Base
+
+# Take the arguments
+mysql_username = sys.argv[1]
+database_name = sys.argv[2]
+state_name_to_search = sys.argv[3]
+
+# Accesing the MetaData
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'.
+    format(mysql_username, "", database_name), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+# create the session
+session = Session(engine)
+
+# The query: the State object with the name passed as
+# argument from the database hbtn_0e_6_usa
+
+query = session.query(State).filter(State.name == state_name_to_search).first()
+# state name to search exist ?
+if query:
+    print(query.id)
+else:
+    print('Not found')
+
+session.close()
