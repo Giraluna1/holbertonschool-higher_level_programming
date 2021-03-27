e9463d6fde2fb4fc9ed4dfc46adea7685cd6d091
Giraluna1 <lunagolo@hotmail.com>
2021-03-27 15:59:08 -0500

    Put main on the top

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,12 +4,13 @@
 import MySQLdb
 import sys
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_to_search = sys.argv[4]
-
 if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_to_search = sys.argv[4]
+
     conn = MySQLdb.connect(host="localHost", port=3306,
                            user=mysql_username, passwd=mysql_password,
                            db=database_name, charset="utf8")
