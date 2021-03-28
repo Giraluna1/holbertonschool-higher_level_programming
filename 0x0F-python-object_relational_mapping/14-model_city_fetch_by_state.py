93975fc19a252f1db07ca7698a8de9b63cb18d5a
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 10:34:29 -0500

    Delete variable of the query

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,10 +6,8 @@ from the database hbtn_0e_14_usa
 
 import sys
 # import the layout of the table instead of Base class
-from model_state import State, Base
+from model_state import Base, State
 from model_city import City
-
-import sqlalchemy
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
@@ -21,19 +19,17 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
 
     # Accesing the MetaData
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.
-        format(mysql_username, mysql_password, database_name),
-        pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(mysql_username, mysql_password, database_name),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     # create the session
     session = Session(engine)
 
     # The query: all city objects
-    full_cities = session.query(City).join(State).order_by(City.id)
 
-    for city in full_cities:
+    for city in session.query(City).order_by(City.id).join(State):
         print(f'{city.state.name}: ({city.id}) {city.name}')
 
     session.close()
