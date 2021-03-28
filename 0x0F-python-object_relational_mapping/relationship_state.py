6d4960e02dea850a7289fa11328baa07f12dec21
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 11:07:46 -0500

    Improve for the relationship with cities

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is the Module Improve the clas state
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_city import City
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ This is the class State iherit from Base"""
+    __tablename__ = "states"
+    id = Column(Integer(), nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City')
