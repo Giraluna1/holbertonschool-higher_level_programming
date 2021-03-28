ec5e95e19bd5692dbbf65b062ff828a9e4f8bbfc
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 10:22:08 -0500

    Change the format of the code

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,16 +5,16 @@ from the database hbtn_0e_14_usa
 """
 
 import sys
+# import the layout of the table instead of Base class
+from model_state import State, Base
+from model_city import City
+
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
-    # import the layout of the table instead of Base class
-    from model_state import State, Base
-    from model_city import City
-
     # Take the arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
@@ -31,11 +31,9 @@ if __name__ == "__main__":
     session = Session(engine)
 
     # The query: all city objects
-    full_cities = session.query(City)\
-        .join(State).order_by(City.id)
+    full_cities = session.query(City).join(State).order_by(City.id)
 
     for city in full_cities:
         print(f'{city.state.name}: ({city.id}) {city.name}')
 
-    session.commit()
     session.close()
