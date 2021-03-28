2a097ad1ea5b3acfde5f9921c478002455f4afc7
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 11:27:37 -0500

    Change the moodule to import

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,7 @@ Module that improve the class City
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from relationship_state import Base, State
 from sqlalchemy.orm import relationship
 
 
