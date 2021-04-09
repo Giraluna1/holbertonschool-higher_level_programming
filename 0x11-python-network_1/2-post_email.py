b7550faa89d12db6329ac4d505bfb53103771c8e
Giraluna1 <lunagolo@hotmail.com>
2021-04-09 18:59:34 -0500

    send post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Use POST an email """
+
+import urllib.parse
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    myobj = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(myobj)
+    data = data.encode('ascii')
+    r = urllib.request.Request(url, data)
+    with urllib.request.urlopen(r) as response:
+        html = response.read()
+        print(html)
