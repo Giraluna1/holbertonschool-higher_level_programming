9770da161ac561120ca531b278938a6b534309ab
Giraluna1 <lunagolo@hotmail.com>
2021-04-09 15:27:54 -0500

    fix pront identation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # opean and read
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- type: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- type: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
