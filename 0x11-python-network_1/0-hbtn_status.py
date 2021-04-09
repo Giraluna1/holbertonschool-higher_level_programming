ffe04a95c07e8e81c0a64f4dc9e872ca2ec28f4b
Giraluna1 <lunagolo@hotmail.com>
2021-04-09 15:29:48 -0500

    Delete coment

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    # opean and read
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
