320c4901c4a5bdf422faf0e38c6b108c16a82efa
Giraluna1 <lunagolo@hotmail.com>
2021-04-09 15:23:51 -0500

    Delete the variable url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
     # opean and read
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(html)))
