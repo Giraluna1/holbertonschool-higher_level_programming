71b316ac02ad2b138976f7a8598b934448507bec
Giraluna1 <lunagolo@hotmail.com>
2021-04-10 21:23:33 -0500

    add on the Try

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,17 +10,18 @@ if __name__ == "__main__":
     else:
         q = sys.argv[1]
 
-   # Create the object to load
+    # Create the object to load
     myobj = {'q': q}
     r = requests.post('http://0.0.0.0:5000/search_user', data=myobj)
     # convert to Json
-    Obj_Json = r.json()
-    # case if Json is empty
-    if len(Obj_Json) == 0:
-        print('No result')
-    # case if Json is load succesfull
-    elif len(Obj_Json) != 0:
-        print('[{}] {}'.format(Obj_Json.get('id'), Obj_Json.get('name')))
+    try:
+        Obj_Json = r.json()
+        # case if Json is empty
+        if len(Obj_Json) == 0:
+            print('No result')
+        # case if Json is load succesfull
+        else:
+            print('[{}] {}'.format(Obj_Json.get('id'), Obj_Json.get('name')))
     # case if pased a invalid json
-    else:
+    except:
         print('Not a valid JSON')
