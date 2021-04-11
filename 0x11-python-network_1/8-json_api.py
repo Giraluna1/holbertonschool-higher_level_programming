e94edb4a51f7fdbd08316dd242f8dac9d6209e3c
Giraluna1 <lunagolo@hotmail.com>
2021-04-10 21:09:32 -0500

    Script that search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" SEARCH API """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if not sys.argv[1]:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+   # Create the object to load
+    myobj = {'q': q}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=myobj)
+    # convert to Json
+    Obj_Json = r.json()
+    # case if Json is empty
+    if len(Obj_Json) == 0:
+        print('No result')
+    # case if Json is load succesfull
+    elif len(Obj_Json) != 0:
+        print('[{}] {}'.format(Obj_Json.get('id'), Obj_Json.get('name')))
+    # case if pased a invalid json
+    else:
+        print('Not a valid JSON')
